3f46fad284393e8927d06a8fec1fab2d8b1863a6
ARIS <aris.sunandar@salt.co.id>
2024-10-04 13:52:03 +0700

    initital project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'try-share-url';
+
+  constructor(meta: Meta){
+    meta.addTags([
+      { property: 'og:title', content: 'TryShareUrl' },
+      { property: 'og:type', content: 'website' },
+      { property: 'og:url', content: 'https://try-share-url.com' },
+      { property: 'og:image', content: 'https://cms.disway.id/uploads/dabf071c490f9961ee4fb03cbc838f30.jpg' },
+      { property: 'og:description', content: 'Try Share URL' },
+    ])
+  }
 }
